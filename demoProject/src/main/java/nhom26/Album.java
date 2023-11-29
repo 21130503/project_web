ee2ee6b840b231f6d86393a18eafe3456be76f5e
= <99793142+HUUQUY1004@users.noreply.github.com>
2023-11-30 00:33:35 +0700

    Chức năng xóa sản phẩm

---
+++ demoProject/src/main/java/nhom26/Album.java
@@ -0,0 +1,50 @@
+package nhom26;
+
+import java.util.ArrayList;
+
+public class Album {
+    int idAlbum;
+    String name;
+    int price;
+    int discount;
+    ArrayList<String> listImage;
+    public int getIdAlbum() {
+        return idAlbum;
+    }
+
+    public void setIdAlbum(int idAlbum) {
+        this.idAlbum = idAlbum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public ArrayList<String> getListImage() {
+        return listImage;
+    }
+
+    public void setListImage(ArrayList<String> listImage) {
+        this.listImage = listImage;
+    }
+}
