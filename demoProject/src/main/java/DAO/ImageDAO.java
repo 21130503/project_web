3021c4bc598badba8d6970b86184dae9b562298a
= <99793142+HUUQUY1004@users.noreply.github.com>
2023-11-30 00:33:35 +0700

    Chức năng xóa sản phẩm

--- demoProject/src/main/java/DAO/ImageDAO.java
+++ demoProject/src/main/java/DAO/ImageDAO.java
@@ -4,6 +4,8 @@ import Services.Connect;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ImageDAO {
@@ -23,6 +25,48 @@ public class ImageDAO {
             }
             return  true;
         } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            Connect.closeConnection(connection);
+        }
+    }
+    public ArrayList<String> getAllImageByIdAlbum(int idAlbum){
+        ArrayList<String> listImage = new ArrayList<>();
+        Connection connection = null;
+        try {
+            connection = Connect.getConnection();
+            String sql = "select source from image where idAlbum= ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1,idAlbum);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                 listImage.add(resultSet.getString("source"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            Connect.closeConnection(connection);
+        }
+        return  listImage;
+    }
+    public  boolean deleteImageBelongAblum(int idAlbum){
+        Connection connection = null;
+        try {
+            connection = Connect.getConnection();
+            String sql = "delete from image where idAlbum = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1,idAlbum);
+            int check = preparedStatement.executeUpdate();
+            if(check >0){
+                return  true;
+            }
+            else{
+                return false;
+            }
+
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         finally {
