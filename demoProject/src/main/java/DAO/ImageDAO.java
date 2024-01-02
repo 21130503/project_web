e7b892583e8323f581f550939c5d802385429631
= <99793142+HUUQUY1004@users.noreply.github.com>
2024-01-03 00:56:49 +0700

    Chinh sua BTS

--- demoProject/src/main/java/DAO/ImageDAO.java
+++ demoProject/src/main/java/DAO/ImageDAO.java
@@ -32,6 +32,31 @@ public class ImageDAO {
             Connect.closeConnection(connection);
         }
     }
+    public boolean addImage(ArrayList<String> sources, int idAlbum){
+        Connection connection =null;
+        if(sources.size() ==0){
+            return  true;
+        }
+        try{
+            connection = Connect.getConnection();
+            String sql = "Insert into Image(idAlbum,source) values (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            for(int i=0; i< sources.size(); i++){
+                preparedStatement.setInt(1, idAlbum);
+                preparedStatement.setString(2, "/images/" + sources.get(i));
+                int check = preparedStatement.executeUpdate();
+                if(check < 0){
+                    return false;
+                }
+            }
+            return  true;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            Connect.closeConnection(connection);
+        }
+    }
     public ArrayList<String> getAllImageByIdAlbum(int idAlbum){
         ArrayList<String> listImage = new ArrayList<>();
         Connection connection = null;
@@ -73,5 +98,34 @@ public class ImageDAO {
         finally {
             Connect.closeConnection(connection);
         }
+    }
+    public boolean updateImageForAlbum(int idAlbum, ArrayList<String> images) {
+        Connection connection = null;
+        for (String source : images) {
+            System.out.println("ImageDAO " + source);
+        }
+        if (images.size() == 0) {
+            return true;
+        }
+        boolean isDelete= true;
+        try {
+
+            connection = Connect.getConnection();
+            String sql = "delete from image where idAlbum = ? and source = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            for (String source : images) {
+                preparedStatement.setInt(1, idAlbum);
+                preparedStatement.setString(2, source);
+                int check = preparedStatement.executeUpdate();
+                if (check <= 0) {
+                    isDelete = false;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            Connect.closeConnection(connection);
+        }
+        return isDelete;
     }
 }
