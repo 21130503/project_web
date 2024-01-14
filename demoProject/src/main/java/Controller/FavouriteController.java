3ea48a8903210f7fc53dc0b7e2841de9a2c08ab6
MinhPHi <21130471@st.hcmuaf.edu.vn>
2024-01-14 08:55:15 +0700

    Edit Error

--- demoProject/src/main/java/Controller/FavouriteController.java
+++ demoProject/src/main/java/Controller/FavouriteController.java
@@ -1,37 +1,5 @@
-package Controller;
-
-<<<<<<< HEAD
-import DAO.OrderDAO;
-import DAO.ProductDAO;
-import DAO.TopicDAO;
-
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
-import java.io.IOException;
-
-@WebServlet(name = "FavouriteController", value = "/FavouriteController")
-public class FavouriteController extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        TopicDAO topicDAO = new TopicDAO();
-        ProductDAO productDAO = new ProductDAO();
-        OrderDAO orderDAO = new OrderDAO();
-        request.setAttribute("listTopic", topicDAO.getAllTopicsForClient());
-        request.setAttribute("listAlbumNew", productDAO.getTop8AlbumNew());
-        request.setAttribute("listOddNew",productDAO.getTop8ddImageNew());
-        request.setAttribute("listOddImageOrder", orderDAO.getTop8OddImageOrder());
-        request.getRequestDispatcher("favourite.jsp").forward(request,response);
-    }
 
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-=======
 import DAO.TopicDAO;
 import favourite.Favourite;
 import nhom26.User;
@@ -117,5 +85,4 @@ public class FavouriteController extends HttpServlet {
             resp.getWriter().write(jsonObject.toString());
         }
     }
->>>>>>> 7d988eedcb7c20d06c678525c619cbcc9a97c9be
 }
